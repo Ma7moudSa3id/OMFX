28625f04097c86fba3e705dabba630f1fd295a68
Eng.MSI <Eng.MSI@DESKTOP-H49GIPU>
2018-08-23 23:33:07 +0200

    added plus button

--- OMFX/src/main/java/com/msi/omfx/ui/controller/HomeController.java
+++ OMFX/src/main/java/com/msi/omfx/ui/controller/HomeController.java
@@ -5,8 +5,10 @@
  */
 package com.msi.omfx.ui.controller;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXHamburger;
 import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
+import com.jfoenix.transitions.hamburger.HamburgerBasicCloseTransition;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.animation.KeyFrame;
@@ -17,6 +19,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
 import javafx.util.Duration;
 
 /**
@@ -36,16 +39,19 @@ public class HomeController implements Initializable {
     @FXML
     private JFXHamburger menuButton;
     private boolean menuPressed = false;
+    JFXButton button1 = new JFXButton("button 1");
+    JFXButton button2 = new JFXButton("button 2");
+    JFXButton button3 = new JFXButton("button 3");
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
         initMenuButton();
-
+        initSideBar();
     }
 
     public void initMenuButton() {
-        HamburgerBackArrowBasicTransition burgerTask = new HamburgerBackArrowBasicTransition(menuButton);
+        HamburgerBasicCloseTransition burgerTask = new HamburgerBasicCloseTransition(menuButton);
         burgerTask.setRate(-1);
         menuButton.addEventHandler(MouseEvent.MOUSE_PRESSED, (e) -> {
             burgerTask.setRate(burgerTask.getRate() * -1);
@@ -55,33 +61,50 @@ public class HomeController implements Initializable {
 
     }
 
+    public void initSideBar() {
+        button1.setPrefSize(300, 75);
+        button1.setButtonType(JFXButton.ButtonType.FLAT);
+        button1.setRipplerFill(Paint.valueOf("#000000"));
+        button1.setTextFill(Paint.valueOf("#7c8184"));
+        button1.getStyleClass().add("sideMenuButton");
+
+        button2.setPrefSize(300, 75);
+        button2.setButtonType(JFXButton.ButtonType.FLAT);
+        button2.setRipplerFill(Paint.valueOf("#000000"));
+        button2.setTextFill(Paint.valueOf("#7c8184"));
+        button2.getStyleClass().add("sideMenuButton");
+
+        button3.setPrefSize(300, 75);
+        button3.setButtonType(JFXButton.ButtonType.FLAT);
+        button3.setRipplerFill(Paint.valueOf("#000000"));
+        button3.setTextFill(Paint.valueOf("#7c8184"));
+        button3.getStyleClass().add("sideMenuButton");
+
+        sideBar.getChildren().add(button1);
+        sideBar.getChildren().add(button2);
+        sideBar.getChildren().add(button3);
+    }
+
     public void toggleSideMenu() {
         if (sideBar.getLayoutX() == 0) {
 
-            //AnchorPane.setLeftAnchor(sideBar, -300.0);
-            //sideBar.setVisible(false);
             KeyValue kv1 = new KeyValue(sideBar.layoutXProperty(), -300);
             KeyValue kv2 = new KeyValue(menuButton.layoutXProperty(), 15);
-            KeyFrame kf1 = new KeyFrame(Duration.millis(500), kv1);
-            KeyFrame kf2 = new KeyFrame(Duration.millis(500), kv2);
+            KeyFrame kf1 = new KeyFrame(Duration.millis(250), kv1);
+            KeyFrame kf2 = new KeyFrame(Duration.millis(250), kv2);
             Timeline timeline = new Timeline();
             timeline.getKeyFrames().add(kf1);
             timeline.getKeyFrames().add(kf2);
             timeline.play();
-//            sideBar.setLayoutX(-300);
-//            menuButton.setLayoutX(15);
         } else {
-            //sideBar.setVisible(true);
             KeyValue kv1 = new KeyValue(sideBar.layoutXProperty(), 0);
             KeyValue kv2 = new KeyValue(menuButton.layoutXProperty(), 250);
-            KeyFrame kf1 = new KeyFrame(Duration.millis(500), kv1);
-            KeyFrame kf2 = new KeyFrame(Duration.millis(500), kv2);
+            KeyFrame kf1 = new KeyFrame(Duration.millis(250), kv1);
+            KeyFrame kf2 = new KeyFrame(Duration.millis(250), kv2);
             Timeline timeline = new Timeline();
             timeline.getKeyFrames().add(kf1);
             timeline.getKeyFrames().add(kf2);
             timeline.play();
-//            sideBar.setLayoutX(0);
-//            menuButton.setLayoutX(250);
         }
     }
 
